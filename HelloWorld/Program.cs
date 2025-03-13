149ee0a7621fcba7f2bf67342f9faae5d3ac86d5
bennyasi64@gmail.com <bennyasi64@gmail.com>
2025-03-13 14:31:56 +0100

    Initial commit - Added C# HelloWorld project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)  
+    {
+        Console.Write("What is your first name? ");
+        string? firstName = Console.ReadLine() ?? "";  // Ensures it's not null
+
+        Console.Write("What is your last name? ");
+        string? lastName  = Console.ReadLine() ?? "";  // Ensures it's not null
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}
