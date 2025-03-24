1258ebaf56f563a7826eb60912b126a8ee6347d1
bennyasi64@gmail.com <bennyasi64@gmail.com>
2025-03-25 00:48:12 +0100

    Initial commit - adding week3 project files

---
+++ ScriptureMemorizer/word.cs
@@ -0,0 +1,21 @@
+public class Word
+{
+    private string text;
+    public bool IsHidden { get; private set; }
+
+    public Word(string text)
+    {
+        this.text = text;
+        IsHidden = false;
+    }
+
+    public void Hide()
+    {
+        IsHidden = true;
+    }
+
+    public string GetDisplayText()
+    {
+        return IsHidden ? "_____" : text;
+    }
+}
