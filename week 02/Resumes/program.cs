144b4fd5eb4c9e7ea4e4b1c9cb9f35a054e99563
bennyasi <bennyasi64@gmail.com>
2025-04-05 04:02:38 +0100

    Add files via upload

---
+++ week 02/Resumes/program.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World! This is the Resumes Project.");
+    }
+}
+public class Job 
+{public double _resposibilties;
+  public double _behaviors;
+
+}
+
+public class Resumes
+{public double _resposibilties;
+  public double _behaviors;
+    
+}
